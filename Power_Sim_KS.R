ff2d8536dd4ec1e5457ac99862f464863de2a5e0
barakbri <barak_b@yahoo.com>
2018-02-22 12:01:43 +0200

    Add comments for KS simulation and Readme

--- Power_Sim_KS.R
+++ Power_Sim_KS.R
@@ -59,37 +59,45 @@ Scenario_names = c('Normal, Shift', 'Mixture, 2 Components', 'Mixture, 3 Compone
 
 #Subsection B: Declarations
 #***********************
-NR_SCENARIOS = length(Scenario_list)
-NULL_TABLE_SIZE = 1000
+NR_SCENARIOS = length(Scenario_list) # number of scenarios in the simulation
+NULL_TABLE_SIZE = 1000 # the null table size
 POWER_REPETITIONS = 2000 #Number of realizations for power evaluation
 PERMUTATIONS_FOR_TEST = NULL_TABLE_SIZE #Number of permutations for tests that require permutations
-MMAX_FOR_KS = 10
-alpha = 0.05
-N1=N2 = 1000
-N = N1+N2
-BENCHMARK_REPETITION = 100
+MMAX_FOR_KS = 10 #parameter for mmax
+alpha = 0.05 #Type I error level for rejection
+N1=N2 = 1000 # sample size is two groups, each of size 1000
+N = N1+N2 #total sample size
+BENCHMARK_REPETITION = 100 #number of repetitions for measurement of running times
 
 #MODES:
-MODE_SUBSECTION_KS_C_PLOT_SETTINGS = TRUE
-MODE_SUBSECTION_KS_D_GENERATE_NULL_TABLES = FALSE
-MODE_SUBSECTION_KS_E_MEASURE_TIMES = FALSE
-MODE_SUBSECTION_KS_F_RUN_SCENARIOS = FALSE
-MODE_SUBSECTION_KS_G_ANALYZE_RESULTS = TRUE
+MODE_SUBSECTION_KS_C_PLOT_SETTINGS = TRUE # Subsection is used for plotting settings
+MODE_SUBSECTION_KS_D_GENERATE_NULL_TABLES = FALSE # Subsection used for generating null tables
+MODE_SUBSECTION_KS_E_MEASURE_TIMES = FALSE #Subsection used for meauring running times
+MODE_SUBSECTION_KS_F_RUN_SCENARIOS = FALSE #Subsection used for running the power simulation, for the different scenraio (multiple cores).
+MODE_SUBSECTION_KS_G_ANALYZE_RESULTS = TRUE #Subsection used for analyzing rersults and plotting them
 
 #Subsection C: Plot Settings 
 #***********************
 if(MODE_SUBSECTION_KS_C_PLOT_SETTINGS){
+  # we plot the scenarios by sampling two milltion obs from each setting, and then doing a kernel estimation of building the density.
+  
+  #sample first setting
   plot_data = Scenario_KS_Normal(10^6,10^6) #sample 2*10^6 points
   dt_gg_normal = data.frame(X = plot_data$x,Group = factor(plot_data$y,labels = c('Group 1','Group 2')),Setting = Scenario_names[1])
+  #sample second setting
   plot_data = Scenario_KS_Normal_Mixture_2_Components(10^6,10^6) #sample 2*10^6 points
   dt_gg_normalcont = data.frame(X = plot_data$x,Group = factor(plot_data$y,labels = c('Group 1','Group 2')),Setting = Scenario_names[2])
+  #sample third setting
   plot_data = Scenario_KS_Mixture(10^6,10^6) #sample 2*10^6 points
   dt_gg_mixture = data.frame(X = plot_data$x,Group = factor(plot_data$y,labels = c('Group 1','Group 2')),Setting = Scenario_names[3])
+  #combine results
   dt_gg = rbind(dt_gg_normal,dt_gg_normalcont,dt_gg_mixture)
+  
   #x_plot_min = -10
   #x_plot_max = 7.5
   #dt_gg = dt_gg[dt_gg$X >= x_plot_min & dt_gg$X <= x_plot_max,]
   
+  #plot the graph, different facet for each scenario
   PLOT_TO_FILE = T
   if(PLOT_TO_FILE){
     pdf('TwoSample-Settings.pdf',height = 2.7,width = 8)
@@ -106,13 +114,13 @@ if(MODE_SUBSECTION_KS_C_PLOT_SETTINGS){
 #***********************
 if(MODE_SUBSECTION_KS_D_GENERATE_NULL_TABLES){
   set.seed(1)
-  
+  #we generate null tables for the MinP procedure, for the Sm statistics. null tables differ by number of atoms, 25,50,100,150
   null_tables = list()
   null_tables[[1]] = HHG::hhg.univariate.ks.nulltable(c(N1,N2),variant = 'KSample-Equipartition', nr.atoms = 25,mmax = MMAX_FOR_KS, nr.replicates = NULL_TABLE_SIZE)
   null_tables[[2]] = HHG::hhg.univariate.ks.nulltable(c(N1,N2),variant = 'KSample-Equipartition', nr.atoms = 50,mmax = MMAX_FOR_KS, nr.replicates = NULL_TABLE_SIZE)
   null_tables[[3]] = HHG::hhg.univariate.ks.nulltable(c(N1,N2),variant = 'KSample-Equipartition', nr.atoms = 100,mmax = MMAX_FOR_KS, nr.replicates = NULL_TABLE_SIZE)
   null_tables[[4]] = HHG::hhg.univariate.ks.nulltable(c(N1,N2),variant = 'KSample-Equipartition', nr.atoms = 150,mmax = MMAX_FOR_KS, nr.replicates = NULL_TABLE_SIZE)
-  
+  #save results to file
   names(null_tables) = c('Sm 25 Atoms','Sm 50 Atoms', 'Sm 100 Atoms','Sm 150 Atoms')
   save(null_tables,file = 'KS_NULL_TABLES.RData')
 }
@@ -121,23 +129,30 @@ if(MODE_SUBSECTION_KS_D_GENERATE_NULL_TABLES){
 #Subsection E: Measure Times
 #***********************
 if(MODE_SUBSECTION_KS_E_MEASURE_TIMES){
-  
+  # we generate data, used for testing
   set.seed(1)
   data = Scenario_KS_Mixture(N1,N2)
   
+  
+  #This is a wrapper for measuring times for the MinP procedure. It receives as a parameter the number of atoms required.
+  #time measurement is for the procedure as a whole: Null table construction, including permuatations and tabulation, computation of test statistic and P.value
   microbenchmark_wrapper_Sm = function(n_atoms){
     res = HHG::hhg.univariate.ks.combined.test(data$x,data$y,variant = 'KSample-Equipartition',nr.atoms = n_atoms,mmax = MMAX_FOR_KS,nr.perm = NULL_TABLE_SIZE)
   }
-  
+
+  #wrapper for the KMMD test  
   microbenchmark_wrapper_KMMD = function(){
     kmmd_res = kernlab::kmmd(matrix(data$x[1:N1],ncol = 1)
                              ,matrix(data$x[(N1+1):N],ncol = 1),asymptotic = TRUE,ntimes = PERMUTATIONS_FOR_TEST)
   }
   
+  #wrapper for the energy test
   microbenchmark_wrapper_ENERGY = function(){
     energy_res = energy::eqdist.etest(data$x,sizes = c(N1,N2),distance = FALSE,R=PERMUTATIONS_FOR_TEST)
   }
   
+  
+  #run the different wrappers, and measure times.
   print('Measuring times for Sm - 25 Atoms')
   mcb_Sm_25 = rbenchmark::benchmark(microbenchmark_wrapper_Sm(25),replications = BENCHMARK_REPETITION)
   print('Measuring times for Sm - 50 Atoms')
@@ -151,6 +166,7 @@ if(MODE_SUBSECTION_KS_E_MEASURE_TIMES){
   print('Measuring times for kmmd')
   mcb_KMMD = rbenchmark::benchmark(microbenchmark_wrapper_KMMD(),replications = BENCHMARK_REPETITION)
   
+  #save run times to disk
   run_times = c(
     Sm_25 = mcb_Sm_25$elapsed/BENCHMARK_REPETITION,
     Sm_50 = mcb_Sm_50$elapsed/BENCHMARK_REPETITION,
@@ -166,38 +182,49 @@ if(MODE_SUBSECTION_KS_E_MEASURE_TIMES){
 }
 
 
-#Subsection F: Run Scenarios
+#Subsection F: Run Scenarios (multiple cores)
 #***********************
 if(MODE_SUBSECTION_KS_F_RUN_SCENARIOS){
-  
+  #Place holder for simulation results
   Simulation_results = NULL
   
+  #This is the worker function run on each of the worker nodes
+  # is receives at a parameter the scenario ID to be run
   POWER_SIM_WORKER_FUNCTION = function(SCENARIO_ID){
+    #this is a new R session, we load all packages and null tables that we need
     library(kernlab)
     library(energy)
     library(HHG)
     
+    #load null tables
     load(file = 'KS_NULL_TABLES.RData') #=> null_tables
 
     NR_REPS_PER_WORKER = ceiling(POWER_REPETITIONS/NR.WORKERS)
     result_names = c('REPS',names(null_tables),'ENERGY','KMMD')
     results = matrix(0,nrow = 1,ncol = length(result_names))     
     colnames(results) = result_names
+    #for each of the required repetitions, PER WORKER
     for(i in 1:NR_REPS_PER_WORKER){
+      #we generate data for the required scenario
       data = Scenario_list[[SCENARIO_ID]](N1,N2)
       x = data$x
       y = data$y
       
+      #we increment the number of tries by 1
       results[1,1] = results[1,1] + 1
       
+      #We iterate over the null tables we generated, and test for rejection. note that all test parameters are stored in the null table.
       for(j in 1:length(null_tables)){
         MinP_res = hhg.univariate.ks.combined.test(x,y,null_tables[[j]])
         results[1,j+1] = results[1,j+1] + 1 *( MinP_res$MinP.pvalue<=alpha)
       }
 
+      # We test using the energy statistic
       energy_res = energy::eqdist.etest(x,sizes = c(N1,N2),distance = FALSE,R=PERMUTATIONS_FOR_TEST)
       results[1,length(result_names)-1] = results[1,length(result_names)-1] + 1 *( energy_res$p.value<=alpha)
       
+      
+      # we test using the kmmd test
       kmmd_res = kernlab::kmmd(matrix(x[y==0],ncol = 1)
                                ,matrix(x[y==1],ncol = 1),asymptotic = TRUE,ntimes = PERMUTATIONS_FOR_TEST)
       results[1,length(result_names)] = results[1,length(result_names)] + 1 *( kmmd_res@AsympH0)
@@ -205,23 +232,32 @@ if(MODE_SUBSECTION_KS_F_RUN_SCENARIOS){
     return(results)
   }
   
+  #these are the scenarios to be run. By default, we run all scenarios
   Scenarios_to_run = 1:NR_SCENARIOS
+  
+  #for each of the required scenarios:
   for(SCENARIO_ID_FOR_SIM  in Scenarios_to_run){
     print(paste0('Running Scenario ID ',SCENARIO_ID_FOR_SIM))
+    
+    #we start a cluster of R workers on the machine
     NR.WORKERS = NR_CORES
     cl <- makeCluster(NR.WORKERS)
     registerDoParallel(cl)
     SCENARIO_SIM_RESULTS <- foreach(i=1:NR.WORKERS, .options.RNG=1234,.combine = 'rbind') %dorng% { POWER_SIM_WORKER_FUNCTION(SCENARIO_ID_FOR_SIM)}
     stopCluster(cl)
     sim_results_as_row = apply(SCENARIO_SIM_RESULTS,2,sum)
+    #we save results
     save(sim_results_as_row,file = paste0('sim_results_as_row_',SCENARIO_ID_FOR_SIM,'.RData'))
     Simulation_results = rbind(Simulation_results,sim_results_as_row) 
   }
-  
+  #we convert the number of rejections to power.
+  # the number of actial repetitions performed might be larger than required, since number of actual repetitions is a complete multiple of number of workers
   Power_results = Simulation_results
   for(i in 1:nrow(Power_results))
     Power_results[i,] = Power_results[i,] / Power_results[i,1]
   rownames(Power_results) = Scenario_names[Scenarios_to_run]
+  
+  #we save the power results to disk
   save(Power_results,file = paste0('KS_Power_Results.RData'))
   
 }
@@ -229,9 +265,11 @@ if(MODE_SUBSECTION_KS_F_RUN_SCENARIOS){
 #Subsection G: Analyze Results
 #***********************
 if(MODE_SUBSECTION_KS_G_ANALYZE_RESULTS){
+  #we load power results and running times
   load(file = 'KS_Power_Results.RData') # => Power_results
   load(file = 'SIMULATION_RUN_TIMES_KS.RData') #=> run_times
   
+  #we organize data in a long format, as preffered by GGPLOT2
   colnames(Power_results) = c("REPS","25 Atoms","50 Atoms", "100 Atoms","150 Atoms","ENERGY","KMMD" )
   #run_times
   power_res_plot = data.frame(Test = NA, Scenario = NA, RunTime = NA, Power = NA)
@@ -254,6 +292,7 @@ if(MODE_SUBSECTION_KS_G_ANALYZE_RESULTS){
   
   power_res_plot$Scenario <- factor(as.character(power_res_plot$Scenario), levels = Scenario_names)
   
+  #we plot the result, log run time by power. Each scenario is given a different facet.
   pdf('KS_PowerResults.PDF', width = 8.5, height = 2.40)
   
   ggplot(power_res_plot) + geom_point(aes(x = log(RunTime) , y = Power ,shape = Test,color = Test))+facet_wrap(~Scenario) +
--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # HHG_large_sample_framework
-Code base and framework for the simulations and results of the large sample HHG paper
+This Git repository houses the Code and framework for the simulations and results of the paper regarding the nonparametric test of independence  given in $\verb|Fast.independence.test|$. This test is computationally efficient for large sample sizes.
+
+## Directory structure
+
+The Script files are as follows:
+
+* $\verb|brill-heller-heller.R|$ - This is the index file running all other files. This script also shows the usage examples detailed in the paper and used to produce the example outputs for the test. The script also graphs the ADP plot used for the description of the algorithm used in the paper. 
+
+* $\verb|Power_Sim_Ind.R|$ - This simulation script runs the power simulations for large sample sizes, comparing the $MinP$/$S^{m\times l}$ test to other tests of independence available on CRAN. The script produces graphs for the different setting, along with run time and power measurements for different sample sizes. Plots produced for results include power by logarithm of runtime, for the different scenarios (and sample sizes)  and power by different sample sizes.
+
+* $\verb|Power_Sim_KS.R|$ - This simulation script runs the power simulations for large sample sizes, for the K-sample tets. The script plots the different scenarios, along with a graph for power by logarithm of runtime.
+
+* $\verb|TimeMeasurements_Comparison_To_Other_Methods.R|$ - The script runs simulations measuring times for the different tests for different sample sizes. An additional simulation measures the run time of the test of a different number of Atoms (method resolution parameter, setting a tradeoff between runtime and computational complexity)
+
+* $\verb|PowerSimulation_SmallN.R|$ - This script performs a power simulation for small sample sizes ($N = 10, 20, 30, 40$), comparing the power of the method to the spearman test.
+
+Other files in the directory are the simulation results and graphs
+
+## Reproducability
+All simulation results and graphs used in producing the paper, are available in this repository.
+Simulation flags (in each file) are configured such that if scripts are run, long simulations (taking hours/ days) are loaded from memory (and not rerun), All graphs and figures are fully reproduced.
+To rerun simualtions, change the $\verb|MODE|$ flags at the top of each file to $\verb|TRUE|$.
+
+Power simulations were run on a C5.18XL amazon machine with 72 cores. The number of cores in each simulation is set according to the machine parameters it is run on. To fully reproduce paper result, please set number of cores to 72. Running times were measured on a single core.
+On a different machine, running times may differ.
+
+## Further Reading
+
+* A detailed vignette is available in our CRAN packge. Install the latest packge using $\verb|install.packages('HHG')|$ using R.
+The vignette is available using $\verb|vignette('HHG')|$
+
+* Further simulations and theory regarding the method for nonparametric independence testing, is available in papers published:
+
+1. Heller, R., Heller, Y., and Gorfine, M. (2013). A consistent multivariate test of association based on ranks of distances. Biometrika, 100(2), 503-510.
+
+2. Heller, R., Heller, Y., Kaufman S., Brill B, & Gorfine, M. (2016). Consistent Distribution-Free K-Sample and Independence Tests for Univariate Random Variables, JMLR 17(29):1-54 http://www.jmlr.org/papers/volume17/14-441/14-441.pdf
+
+3. Brill B. (2016) Scalable Non-Parametric Tests of Independence (master's thesis). http://primage.tau.ac.il/libraries/theses/exeng/free/2899741.pdf
